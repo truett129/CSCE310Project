31d0693f02ef66954751169b3673b99b7eee9524
Truett Lee <truett129lee@gmail.com>
2023-11-15 11:57:56 -0600

    added basic php and style sheets

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Homepage</title>
+    <link rel="stylesheet" href="css/styles.css"/>
+</head>
+<body>
+    <div id="content">
+        <h1>Welcome to My Homepage</h1>
+        <p>This is a simple homepage created using HTML, CSS, PHP, and MySQL.</p>
+        <!-- PHP code can be inserted here to display dynamic content -->
+    </div>
+</body>
+</html>
