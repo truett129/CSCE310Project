96dd22c02130bef6ac4a17449ea855598c440a91
aciardelli <anthciardelli@gmail.com>
2023-11-29 08:57:40 -0600

    view application finished

--- student/s_application_info.php
+++ student/s_application_info.php
@@ -3,13 +3,13 @@
     $result = mysqli_query($conn,"SELECT Program_Num, Name FROM programs");
 
     // insert
-    if(isSet($_POST['uin']) && isSet($_POST['program-name']) && isSet($_POST['purpose-statement'])){
+    if(isSet($_POST['uin']) && isSet($_POST['program-num']) && isSet($_POST['purpose-statement'])){
         $uin = $_POST['uin'];
-        $program_name = $_POST['program-name'];
+        $program_num = $_POST['program-num'];
         $uncom_cert = $_POST['uncom-cert'];
         $com_cert = $_POST['com-cert'];
         $purpose_statement = $_POST['purpose-statement'];
-        $sql = "INSERT INTO `applications` (`uin`, `program_name`, `uncom_cert`, `com_cert`, `purpose_statement`) VALUES ('$uin', '$program_name', '$uncom_cert', '$com_cert', '$purpose_statement')";
+        $sql = "INSERT INTO `applications` (`uin`, `program_num`, `uncom_cert`, `com_cert`, `purpose_statement`) VALUES ('$uin', '$program_num', '$uncom_cert', '$com_cert', '$purpose_statement')";
         if(mysqli_query($conn, $sql)){
             $message = "New record created successfully";
         } else {
@@ -24,40 +24,91 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    <?php include '../css/styles.css'; ?>
+    </style>
     <title>Student Application Information</title>
 </head>
 <body>
     <h1>Student Application Information</h1>
-    <form action="" method="POST">
-        <label for="uin">UIN</label>
-        <input type="text" name="uin" id="uin" required>
+    <div class="student-application-form">
+        <h2>New Application Form</h2>
+        <form action="" method="POST">
+            <div class="input-label">
+                <label for="uin">UIN</label>
+                <input type="text" name="uin" id="uin" required>
+            </div>
+            <div class="input-label">
+                <label for="program-num">Program Name</label>
+                <select name="program-num" id="program-num" required>
+                    <?php
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<option value='" . $row['Program_Num'] . "'>" . $row['Name'] . "</option>";
+                            }
+                        }
+                    ?>
+                </select>
+            </div>
+    
+            <div class="input-label">
+                <label for="uncom-cert">Are you currently enrolled in
+                other uncompleted certifications
+                sponsored by the Cybersecurity
+                Center? (Leave blank if no)</label>
+                <input type="text" name="uncom-cert" id="uncom-cert">
+            </div>
+    
+            <div class="input-label">
+                <label for="com-cert">Have you completed any
+                cybersecurity industry
+                certifications via the
+                Cybersecurity Center? (Leave blank if no)</label>
+                <input type="text" name="com-cert" id="com-cert">
+            </div>
+    
+            <div class="input-label">
+                <label for="purpose-statement">Purpose Statement</label>
+                <input type="text" name="purpose-statement" id="purpose-statement" required>
+            </div>
 
-        <label for="program-name">Program Name</label>
-        <select name="program-name" id="program-name" required>
-            <?php
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<option value='" . $row['Program_Num'] . "'>" . $row['Name'] . "</option>";
-                    }
+    
+            <input type="submit" name='submit' value="Submit">
+        </form>
+        <?php if(!empty($message)): ?>
+            <div class="message">
+                <?php echo $message; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+    <h2>My Applications</h2>
+    <table>
+        <tr>
+            <th>Program Name</th>
+            <th>Status</th>
+            <th>Update</th>
+            <th>Select</th>
+            <th>Delete</th>
+        </tr>
+        <?php
+            $userUIN = 230002124;
+            $result = mysqli_query($conn, "SELECT applications.*, programs.Name FROM applications INNER JOIN programs ON applications.Program_Num = programs.Program_Num WHERE applications.uin = $userUIN");
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>
+                    <td>" . $row['Name'] . "</td>
+                    <td>N/A</td>
+                    <td><a href='update_application.php?program_num=" . $row['Program_Num'] . "&uin=" . $userUIN . "'>Update</a></td>
+                    <td><a href='select_application.php?program_num=" . $row['Program_Num'] . "&uin=" . $userUIN . "'>Select</a></td>
+                    <td><a href='delete_application.php?program_num=" . $row['Program_Num'] . "&uin=" . $userUIN . "'>Delete</a></td>
+                    </tr>";
                 }
-            ?>
-        </select>
-
-        <label for="uncom-cert">Are you currently enrolled in
-        other uncompleted certifications
-        sponsored by the Cybersecurity
-        Center? (Leave blank if no)</label>
-        <input type="text" name="uncom-cert" id="uncom-cert">
-
-        <label for="com-cert">Have you completed any
-        cybersecurity industry
-        certifications via the
-        Cybersecurity Center? (Leave blank if no)</label>
-        <input type="text" name="com-cert" id="com-cert">
-
-        <label for="purpose-statement">Purpose Statement</label>
-        <input type="text" name="purpose-statement" id="purpose-statement" required>
-
-        <input type="submit" name='submit' value="Submit">
+            }
+            else{
+                echo "<tr><td colspan='4'>No applications found</td></tr>";
+            }
+        ?>
+    </table>
+    <button><a href="s_application_info.php">Refresh</a></button>
 </body>
 </html>
