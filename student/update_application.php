c8f30a2fb7c76efc42b09f11f1f2e14490b20e68
aciardelli <anthciardelli@gmail.com>
2023-12-01 08:28:24 -0600

    Update functionality for student applications

--- student/update_application.php
+++ student/update_application.php
@@ -1,11 +1,9 @@
 <?php
     include_once '../database.php';
     if(count($_POST)>0){
-        mysqli_query($conn, "UPDATE applications SET uin='" . $_POST['uin'] . "', program_num='" . $_POST['program-num'] . "', uncom_cert='" . $_POST['uncom-cert'] . "', com_cert='" . $_POST['com-cert'] . "', purpose_statement='" . $_POST['purpose-statement'] . "' WHERE uin='" . $_POST['uin'] . "'");
+        mysqli_query($conn, "UPDATE applications SET Uncom_Cert='" . $_POST['uncom_cert'] . "', Com_Cert='" . $_POST['com_cert'] . "', Purpose_Statement='" . $_POST['purpose_statement'] . "' WHERE Program_Num='" . $_GET['program_num'] . "' AND UIN='" . $_GET['uin'] . "'");
         $message = "Record Modified Successfully";
     }
-    $result = mysqli_query($conn, "SELECT * FROM applications WHERE uin='" . $_GET['uin'] . "'");
-    $row= mysqli_fetch_array($result);
 ?>
 
 <!DOCTYPE html>
@@ -19,54 +17,65 @@
 <body>
     <h1>Update Application</h1>
     <div class="student-application-form">
-        <h2>New Application Form</h2>
         <form action="" method="POST">
             <div class="input-label">
                 <label for="uin">UIN</label>
                 <input type="text" name="uin" id="uin" value=<?php echo $_GET['uin'] ?> readonly>
             </div>
             <div class="input-label">
-                <label for="program-num">Program Name</label>
-                <select name="program-num" id="program-num" required>
-                    <?php
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<option value='" . $row['Program_Num'] . "'>" . $row['Name'] . "</option>";
-                            }
-                        }
-                    ?>
+            <label for="program-name">Program Name</label>
+            <input type="text" name="program_name" id="program_name" value="<?php
+
+                $programNum = mysqli_real_escape_string($conn, $_GET['program_num']);
+                $query = "SELECT Name FROM programs WHERE Program_Num = '$programNum'";
+                $result = mysqli_query($conn, $query);
+                
+                if ($result) {
+                    $row = mysqli_fetch_assoc($result);
+                    
+                    if ($row) {
+                        echo htmlspecialchars($row['Name']);
+                    } else {
+                        echo 'No program found';
+                    }
+                } else {
+                    echo 'Error fetching program';
+                }
+            ?>" readonly required>
+
                 </select>
             </div>
+            <?php
+                $uin = $_GET['uin'];
+                $programNum = $_GET['program_num'];
+                $result = mysqli_query($conn,"SELECT * FROM applications WHERE uin=$uin AND program_num=$programNum");
+                if ($result) {
+                    $row = mysqli_fetch_assoc($result);
+                }
+            ?>
     
             <div class="input-label">
-                <label for="uncom-cert">Are you currently enrolled in
+                <label for="uncom_cert">Are you currently enrolled in
                 other uncompleted certifications
                 sponsored by the Cybersecurity
                 Center? (Leave blank if no)</label>
-                <input type="text" name="uncom-cert" id="uncom-cert">
+                <input type="text" name="uncom_cert" id="uncom_cert" value="<?php echo isset($row['Uncom_Cert']) ? $row['Uncom_Cert'] : ''; ?>" >
             </div>
     
             <div class="input-label">
-                <label for="com-cert">Have you completed any
+                <label for="com_cert">Have you completed any
                 cybersecurity industry
                 certifications via the
                 Cybersecurity Center? (Leave blank if no)</label>
-                <input type="text" name="com-cert" id="com-cert">
+                <input type="text" name="com_cert" id="com_cert" value="<?php echo isset($row['Com_Cert']) ? $row['Com_Cert'] : ''; ?>" >
             </div>
     
             <div class="input-label">
-                <label for="purpose-statement">Purpose Statement</label>
-                <input type="text" name="purpose-statement" id="purpose-statement" required>
+                <label for="purpose_statement">Purpose Statement</label>
+                <input type="text" name="purpose_statement" id="purpose_statement" value="<?php echo isset($row['Purpose_Statement']) ? $row['Purpose_Statement'] : ''; ?>" >
             </div>
-
-    
             <input type="submit" name='submit' value="Submit">
+            <p><?php if(isset($message)) echo $message; ?></p>
         </form>
-        <?php if(!empty($message)): ?>
-            <div class="message">
-                <?php echo $message; ?>
-            </div>
-        <?php endif; ?>
-    </div>
 </body>
 </html>
