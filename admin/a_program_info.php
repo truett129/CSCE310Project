4d4c55c29dee6387ac90d579f3f5b60a7143411a
aciardelli <anthciardelli@gmail.com>
2023-12-08 23:14:30 -0600

    removed comments

--- admin/a_program_info.php
+++ admin/a_program_info.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Program Information</title>
-    <link rel="stylesheet" href="../css/styles.css" /> <!-- Ensure the path to style.css is correct -->
+    <link rel="stylesheet" href="../css/styles.css" />
 </head>
 
 <body>
